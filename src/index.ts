ee503c504690f71b36a04405f4df6b05c469b87d
Prateek Kumar <prateek@prateekkumar.in>
2019-01-06 16:47:37 +0530

    Update config system
    Signed-off-by: Prateek Kumar <prateek@prateekkumar.in>

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,31 @@
+const ArgumentParser = require('argparse').ArgumentParser
+const parser = new ArgumentParser({
+	addHelp: true
+})
+
+parser.addArgument(['-c', '--config'], {
+	help: 'Config file'
+})
+parser.addArgument(['-o', '--output'], {
+	help: 'Output file'
+})
+
+const args = parser.parseArgs()
+
+
 const ical = require('ical-generator')
 
 // Calendar object initialization
-const cal = ical()
-cal.domain('iith.prateekkumar.in')
-cal.name('Timetable @ IITH')
-cal.prodId('//IIT Hyderabad//Timetable Generator//EN')
-cal.timezone('Asia/Kolkata')
-cal.ttl(60)
+const cal = ical({
+	domain: 'iith.prateekkumar.in',
+	prodId: '//IIT Hyderabad//Timetable Generator//EN',
+	name: 'Timetable @ IITH',
+	timezone: 'Asia/Kolkata'
+})
 
 // Load slots, courses, segments and holidays
 const yaml = require('yamljs')
-const slots = yaml.load('../config/slots.yaml')
-const courses = yaml.load('../config/courses.yaml')
-const segments = require('../config/segments.json')
-const holidays = require('../config/holidays.json').map((s: string)=>{return new Date(s)})
+const config = yaml.load(args.config)
 
 // Function to find start and end date time
 // First find the next date on or after given @date
@@ -32,8 +44,9 @@ let findStartAndEndTime = (date_str: string, day_str: string, times: any[]) => {
 }
 
 // For all courses, create events in the calendar object
-Object.keys(courses).forEach((id)=>{
-	let course = courses[id]
+Object.keys(config.courses).forEach((id)=>{
+	let course = config.courses[id]
+	id = course.code
 
 	// Find start and end segments
 	// For eg. if segment given is 14, then startSegment = 0 and endSegment = 4(exclusive)
@@ -41,39 +54,44 @@ Object.keys(courses).forEach((id)=>{
 	let endSegment = Number(String(course['segment'])[1]);
 
 	// Find the slot of the course
-	let slot = slots[course['slot']]
+	let slot = course.slot
+	let slot_name = 'custom'
+
+	if (typeof slot === 'string') {
+		slot_name = slot
+		slot = config.slots[slot]
+	}
 
 	// Iterate over each segment of the current course
 	for (let i=startSegment; i<endSegment; i++) {
-		let segment = segments[i]
+		let segment = config.segments[i]
 
 		// Iterate over all days that the course is scheduled on
 		Object.keys(slot).forEach((day)=>{
 			// Find the start and end time of first day of the course
-			let times = findStartAndEndTime(segment[0], day, slot[day])
+			let times = findStartAndEndTime(segment['start'], day, slot[day])
 			// Find the last day of the segment
-			let lastDate = new Date(segment[1])
+			let lastDate = new Date(segment['end'])
 			lastDate.setHours(23, 59, 59, 0)
 
 			// Create the event with repeating weekly until last date
 			cal.createEvent({
-				uid: id+'_'+lastDate.getFullYear()+'_'+i+'_'+course.slot+'_'+day.substring(0,2),
+				uid: id+'_'+lastDate.getFullYear()+'_'+i+'_'+slot_name+'_'+day.substring(0,2),
 				start: times[0],
 				end: times[1],
 				summary: id+': '+course.name,
-				location: 'CL No. '+course.classroom,
+				location: ''+course.classroom,
 				repeating: {
 					freq: 'WEEKLY',
 					until: lastDate,
-					exclude: holidays
+					exclude: config.holidays
 				}
 			})
 		})
 	}
 }, cal)
 
-let ttFile = '../out/tt.ics'
-if (process.argv.length > 2) ttFile = process.argv[2]
+let ttFile = args.output
 
 // Save the calendar in the iCal format
 cal.save(ttFile, (err: any)=>{
